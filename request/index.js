ab7cf99d11b844d6c508c4eb08e909ad48404c9a
steven-sbconsulting <132986238+steven-sbconsulting@users.noreply.github.com>
2026-06-03 15:40:44 +0200

    Update index.js

--- request/index.js
+++ request/index.js
@@ -1,8 +1,26 @@
+const axios = require("axios");
+
 module.exports = async function (context, req) {
     context.log("Request received");
 
+    let email = req.body?.email || req.query.email;
+    let mac = req.body?.mac || req.query.mac;
+
+    if (!email || !mac) {
+        context.res = {
+            status: 400,
+            body: "Missing email or MAC"
+        };
+        return;
+    }
+
+    await axios.post(process.env.FLOW_URL, {
+        email: email,
+        mac: mac
+    });
+
     context.res = {
         status: 200,
-        body: "Function is working ✅"
+        body: "Request submitted. Waiting for approval..."
     };
 };
