3c3ef7786e00c9e2a4bb2cdb3b032be4cea64d6c
steven-sbconsulting <132986238+steven-sbconsulting@users.noreply.github.com>
2026-06-03 15:26:49 +0200

    Update index.js
    test

--- request/index.js
+++ request/index.js
@@ -1,14 +1,8 @@
-
 module.exports = async function (context, req) {
-    const { email, mac } = req.body;
-
-    await fetch(process.env.FLOW_URL, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, mac })
-    });
+    context.log("Request received");
 
     context.res = {
-        body: "Request submitted. Wait for approval."
+        status: 200,
+        body: "Function is working ✅"
     };
 };
